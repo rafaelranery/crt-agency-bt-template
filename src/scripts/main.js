2cdc7ba9fed72938850c42ac7698fedd0fad944c
rafaelranery <rafaelrapnery@gmail.com>
2023-04-28 15:13:50 -0300

    navbar / hero done

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+/* Navbar mobile functionaly */
+
+$('.h-menu-btn').click((e) => {
+    e.preventDefault;
+    
+    $('.navbar__overlay-menu').addClass('navbar__overlay-menu--active')
+})
+document.querySelectorAll('.navbar__overlay-menu__links__pill__link').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault
+        $('.navbar__overlay-menu').addClass('navbar__overlay-menu--return')
+
+        $('.navbar__overlay-menu').removeClass('navbar__overlay-menu--active')
+        setTimeout(() => {
+            $('.navbar__overlay-menu').removeClass('navbar__overlay-menu--return')
+        }, 900)
+    })
+})
+
+
+
+
+
+
+
+/*  */
